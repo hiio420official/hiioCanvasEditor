3aa7ec5082132bdc8e6ca525815bc5c38fe7e8d9
hiio420official <hiio420.official@gmail.com>
2025-11-05 01:02:20 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header>Header</header>
+      <aside>Aside</aside>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
